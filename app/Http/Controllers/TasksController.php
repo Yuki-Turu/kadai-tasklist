df1a28ead66e8b38b8e4cbe8a3143c56b7344d4b
yuki.t <turuchan124@gmail.com>
2022-03-22 09:23:30 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,12 +45,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'title'=>'required|max:255',
+            'status'=>'required|max:10',
             'content'=>'required|max:255',
             ]);
         
         $task=new Task;
-        $task->title=$request->title;
+        $task->status=$request->status;
         $task->content=$request->content;
         $task->save();
         
@@ -96,11 +96,11 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
-            'title'=>'required|max:255',
+            'status'=>'required|max:10',
             'content'=>'required|max:255',
             ]);
         $task = Task::findOrFail($id);
-        $task->title=$request->title;
+        $task->status=$request->status;
         $task->content = $request->content;
         $task->save();
 
