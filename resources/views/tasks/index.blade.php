7b4645e6756408f9db356a9a6ac9aa0441e1cf76
yuki.t <turuchan124@gmail.com>
2022-03-22 07:50:26 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -14,6 +14,7 @@
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->title }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
